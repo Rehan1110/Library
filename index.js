ff7cf862755f21e60b66d462adc92841235ecff6
Rehan Abbas <abbasrehan054@gmail.com>
2024-02-17 23:38:11 +0530

    Completed the modal part

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 //js
+
+let addBtn = document.querySelector('.add-button');
+let modal = document.querySelector('#modal');
+let modalCloseBtn = document.querySelector('.modal-close-button');
+
+addBtn.addEventListener('click',(e)=>{
+  e.preventDefault();
+  modal.style.display = 'block';
+})
+
+modalCloseBtn.addEventListener('click', (e) =>{
+  e.preventDefault();
+  modal.style.display = 'none';
+})
+
+window.addEventListener('click', (e) => {
+  if(e.target == modal){
+    modal.style.display = 'none';
+  }
+})
+
 const myLibrary = [];
 
 function Book(title, author, pages, ReadOrNot) {
