edd2a0711b410d40b13e5c51397e89def23af6c0
Rehan Abbas <abbasrehan054@gmail.com>
2024-02-18 22:59:04 +0530

    Working on the read and not read button

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 let addBtn = document.querySelector('.add-button');
 let modal = document.querySelector('#modal');
 let modalCloseBtn = document.querySelector('.modal-close-button');
+let ReadValue = document.querySelector('.read-button');
 
 addBtn.addEventListener('click',(e)=>{
   e.preventDefault();
@@ -18,6 +19,11 @@ window.addEventListener('click', (e) => {
   if(e.target == modal){
     modal.style.display = 'none';
   }
+})
+
+ReadValue.addEventListener('click', () =>{
+  console.log("I am clicked");
+  console.log(ReadValue.value);
 })
 
 const myLibrary = [];
